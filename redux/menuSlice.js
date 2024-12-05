3792f63bbfa76bff341ca02e4fca620bbf37ca80
omkar walke <omkarwalke647@gmail.com>
2024-12-05 16:45:21 +0530

    just fixed

--- redux/menuSlice.js
+++ redux/menuSlice.js
@@ -4,6 +4,13 @@ const initialState = {
     items: [
       { id: 1, name: "Pizza", description: "Delicious cheese pizza", image: "/images/pizza.avif",price:200 },
       { id: 2, name: "Burger", description: "Juicy beef burger", image: "/images/burger.avif" ,price:400},
+      { id: 3, name: "Vada Pav", description: "Mumbai cha VadaPav", image: "/images/maxresdefault.jpg" ,price:200},
+      { id: 4, name: "Misal Pav", description: "Puny chi Misal", image: "/images/images.jpg" ,price:100},
+      { id: 5, name: "Thallipeeth", description: "Maharastrian Special Thalipeeth", image: "/images/thalipeeth.jpg" ,price:300},
+
+
+
+
     
       
       
